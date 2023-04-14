c00d16d0870cd59ce0d70625c6c0657f680892cf
Artem Yanchenko <artem.yanchenko.94@mail.ru>
2023-04-14 15:32:32 +0300

    done findShort

--- JadenCasingStrings.js
+++ JadenCasingStrings.js
@@ -1,6 +1,5 @@
 String.prototype.toJadenCase = function (str) {
-    str = this.split(' ').map(el=>el[0].toUpperCase() + el.substring(1)).join(' ')
-    return str
+    return this.split(' ').map(el=>el[0].toUpperCase() + el.substring(1)).join(' ')
 };
 
 console.log(String.prototype.toJadenCase("How can mirrors be real if our eyes aren't real"))
