dc229af5d93ce07721c5ea4474d1652f9dd380ea
Artem Yanchenko <artem.yanchenko.94@mail.ru>
2023-04-15 23:15:47 +0300

    done Testing123

---
+++ Testing123.js
@@ -0,0 +1,9 @@
+var number=function(array){
+    return array.map((el,index)=>`${index+1}: ${el}`);
+}
+
+
+console.log(number(["a", "b", "c"]))
+
+
+// number(["a", "b", "c"]), ["1: a", "2: b", "3: c"]
