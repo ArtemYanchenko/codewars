f5685ad5f13a817ab94abb3693644d221f895769
Artem Yanchenko <artem.yanchenko.94@mail.ru>
2023-04-19 22:39:28 +0300

    done ChainMe

--- 7kata/ChainMe.js
+++ 7kata/ChainMe.js
@@ -1,6 +1,15 @@
 function chain(input, fs) {
 // implement the "chain" function
-    fs[0](fs[1](input))
+    return fs.reduce((input,el)=>{return el(input) },input);
+}
+
+
+function add(x) {
+    return x + 10;
+}
+
+function mult(x) {
+    return x * 30;
 }
 
 console.log(chain(2,[add,mult]))
