713da9d682cbe69c99bf1ee2e98e1e749ca08db9
Artem Yanchenko <artem.yanchenko.94@mail.ru>
2023-04-23 23:40:15 +0300

    edit sumFactorial

--- 7kata/sumFactorial.js
+++ 7kata/sumFactorial.js
@@ -1,15 +1,17 @@
-function sumFactorial(arr){
-    let first = 0
-    let second = 0
-    for (let i = 0; i <= arr[0]; i++) {
-        first *=i
-    }
-    for (let i = 0; i <= arr[1]; i++) {
-        second *=i
-    }
-    return first+second
+function sumFactorial(num) {
+
+    // for (let i = 1; i <= num[0]; i++) {
+    //     first *= i
+    // }
+    // for (let i = 1; i <= num[1]; i++) {
+    //     second *= i
+    // }
+
+    num.map(n=>function factorial(n) {
+        return (n != 1) ? n * factorial(n - 1) : 1;
+    })
 }
 
 
-console.log(sumFactorial([4,6]))
+console.log(sumFactorial([4, 6]))
 // ([4,6]), 744);
