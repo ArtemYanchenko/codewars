1fa24ad87e36ae9aa35d35826522f3dac8f7730b
Artem Yanchenko <artem.yanchenko.94@mail.ru>
2023-04-24 23:16:22 +0300

    edit alienLanguage

--- 7kata/alienLanguage.js
+++ 7kata/alienLanguage.js
@@ -1,8 +1,8 @@
 function alienLanguage(str){
-    //coding here...
+    return str.split(' ').map(el=>el.toUpperCase() + el.substring()).join(' ');
 
 }
 
 
-
+console.log(alienLanguage("My name is John"))
 // ("My name is John") , "My NAMe Is JOHn");
