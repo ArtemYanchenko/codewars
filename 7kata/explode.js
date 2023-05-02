8ac1d8483cbbc9e45930755729070040810064be
Artem Yanchenko <artem.yanchenko.94@mail.ru>
2023-05-02 15:08:47 +0300

    edit in explode

--- 7kata/explode.js
+++ 7kata/explode.js
@@ -1,5 +1,9 @@
 function explode(x){
-
+    let newArr = [];
+    if (typeof(x[0]) === 'number' && typeof(x[1]) === 'number') {
+        return
+    } else if (typeof(x[0]) === 'number' || typeof(x[1]) === 'number') {
+        return
+    }
 }
-
 console.log(explode([9, 3])); // [[9, 3], [9, 3], [9, 3], [9, 3], [9, 3], [9, 3], [9, 3], [9, 3], [9, 3], [9, 3], [9, 3], [9, 3]]);
