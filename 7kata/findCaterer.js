bd7394b7604642dbc9f27554d7a28e3920e40c48
Artem Yanchenko <artem.yanchenko.94@mail.ru>
2023-05-06 23:16:03 +0300

    done findCaterer

---
+++ 7kata/findCaterer.js
@@ -0,0 +1,19 @@
+function findCaterer(budget, people){
+    if (budget < 15 || people < 1) {
+        return -1;
+    }
+
+    if (people * (people > 60 ? 24 : 30) <= budget ) {
+        return 3
+    }
+
+    if (people * 20 <= budget ) {
+        return 2
+    }
+
+    if (people * 15 <= budget ) {
+        return 1
+    }
+
+    return -1;
+}
